df90595225198502dfb9b9be9b5a047d2de0626a
enjay <njokuut@gmail.com>
2020-05-05 20:27:36 -0400

    DevSlopes app take 2

--- DevProfile/app/src/main/java/com/enjaybaze/devprofile/MainActivity.kt
+++ DevProfile/app/src/main/java/com/enjaybaze/devprofile/MainActivity.kt
@@ -1,12 +1,21 @@
 package com.enjaybaze.devprofile
 
+import android.graphics.BitmapFactory
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.graphics.drawable.RoundedBitmapDrawableFactory
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val bitmap = BitmapFactory.decodeResource(resources, R.drawable.devslopesprofilelogo) // this is to modify the profile logo and make the edges more rounded
+        val rounded = RoundedBitmapDrawableFactory.create(resources, bitmap)
+        rounded.cornerRadius = 15f
+//        rounded.isCircular = true // if we wanted to make the logo circular
+        logo.setImageDrawable(rounded)
     }
 }
